96aab11d839b8316c95c9eccba3a58a8ea5d9dce
Даниил Хлыбов <steglaset@gmail.com>
2022-05-24 16:57:04 +0300

    fix: 04-copy-directory now can delete mismathced files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const {
+  constants
+} = require('node:fs');
 
 function copyDir(folder) {
   let distFolder = folder + '-copy';
@@ -7,32 +10,40 @@ function copyDir(folder) {
   fs.promises.mkdir(distFolder, {
     recursive: true
   })
+  .then(() => {
+    fs.promises.rm(distFolder, { recursive: true })
     .then(() => {
-      (function goDeeper(folder, distFolder) {
-        fs.promises.readdir(folder, {
-          withFileTypes: true
-        })
-          .then(files => {
-            for (let file of files) {
-              if (file.isFile()) {
-                let srcPath = path.join(folder, file.name);
-                let distPath = path.join(distFolder, file.name);
-
-                fs.promises.copyFile(srcPath, distPath);
-              } else if (file.isDirectory()) {
-                fs.promises.mkdir(path.join(distFolder, file.name), {
-                  recursive: true
-                })
-                  .then(() => {
-                    goDeeper(path.join(folder, file.name), path.join(distFolder, file.name));
-                  });
+      fs.promises.mkdir(distFolder, {
+        recursive: true
+      })
+      .then(() => {
+        (function goDeeper(folder, distFolder) {
+          fs.promises.readdir(folder, {
+              withFileTypes: true
+            })
+            .then(files => {
+              for (let file of files) {
+                if (file.isFile()) {
+                  let srcPath = path.join(folder, file.name);
+                  let distPath = path.join(distFolder, file.name);
+  
+                  fs.promises.copyFile(srcPath, distPath);
+                } else if (file.isDirectory()) {
+                  fs.promises.mkdir(path.join(distFolder, file.name), {
+                      recursive: true
+                    })
+                    .then(() => {
+                      goDeeper(path.join(folder, file.name), path.join(distFolder, file.name));
+                    });
+                }
               }
-            }
-          })
-          .catch(err => console.error(err));
-      })(folder, distFolder);
-    })
-    .catch(err => console.error(err));
+            })
+            .catch(err => console.error(err));
+        })(folder, distFolder);
+      })
+      .catch(err => console.error(err));
+    });
+  });
 }
 
 copyDir(path.join(__dirname, 'files'));
